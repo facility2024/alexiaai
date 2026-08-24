df9f4b1660bc6a908e7860319ff6df8c593b3bd8
Facility2024 <facility2024@gmail.com>
2026-08-24 14:43:39 -0300

    fix: remove SecurityGuard overlay

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -12,7 +12,7 @@ import { useEffect, type ReactNode } from "react";
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 import { Toaster } from "@/components/ui/sonner";
-import { SecurityGuard } from "@/components/security-guard";
+
 
 function NotFoundComponent() {
   return (
@@ -131,7 +131,6 @@ function RootComponent() {
   return (
     <QueryClientProvider client={queryClient}>
       <Outlet />
-      <SecurityGuard />
       <Toaster richColors position="top-right" />
     </QueryClientProvider>
   );
